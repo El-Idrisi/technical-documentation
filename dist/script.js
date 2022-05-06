ef3e93323212a3a4924e3999d1fe68865ae57aee
El-Idrisi <muhammad.3l.idrisi@gmail.com>
2022-05-06 14:50:03 +0700

    Remake Technical Page

--- dist/script.js
+++ dist/script.js
@@ -1,14 +1,19 @@
 const hambuger = document.querySelector('#hambuger');
-const menuNavbar = document.querySelector('#navbar');
+const navbar = document.querySelector('#navbar');
+const menuNavbar = document.querySelector('#menuNavbar');
 const header = document.querySelector('#header');
 
 
 hambuger.addEventListener('click', function () {
     hambuger.classList.toggle('hambuger-active');
-    menuNavbar.classList.toggle('hidden');
+    navbar.classList.toggle('bg-slate-400');
+    header.classList.toggle('-translate-x-full');
+    menuNavbar.classList.toggle('-translate-x-full');
 })
 
-menuNavbar.addEventListener('click', function () {
-    hambuger.classList.remove('hambuger-active');
-    menuNavbar.classList.add('hidden');
+menuNavbar.addEventListener("click", function(){
+    hambuger.classList.toggle('hambuger-active');
+    navbar.classList.toggle('bg-slate-400');
+    header.classList.toggle('-translate-x-full');
+    menuNavbar.classList.toggle('-translate-x-full');
 })
